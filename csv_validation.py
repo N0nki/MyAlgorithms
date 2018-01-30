7dc2abba35d7f0fcac73b6c769bc84b33db83e04
N0nki <rgstr_land@icloud.com>
2018-01-30 18:13:23 +0900

    add function: csv_validation2

--- csv_validation.py
+++ csv_validation.py
@@ -37,7 +37,7 @@ def validate_csv(filename, comment=False):
             print(states_table[state], char)
             transition_regexp = transitions[state]
             for regexp, s in transition_regexp.items():
-                matched = re.search(regexp, char)
+                matched = re.match(regexp, char)
                 if matched is not None:
                     state = s
                 if state == ERROR:
@@ -46,8 +46,45 @@ def validate_csv(filename, comment=False):
             state = init_state
     return True
 
+def validate_csv2(filename, comment=True):
+    """
+    正規表現を使用しないバージョン
+    """
+    START, VALUE, FIRSTQ, SECONDQ, RETURN, ERROR = range(6)
+    states = ["START", "VALUE", "FIRSTQ", "SECONDQ", "RETURN", "ERROR"]
+    states_table = dict(zip(list(range(6)), states))
+    init_state = START
+    transitions = {
+            START: {",": START, "\"": FIRSTQ, "\n": RETURN, (",", "\\"): VALUE},
+            VALUE: {(","): VALUE, "\n": RETURN, ",": START},
+            FIRSTQ: {("\""): FIRSTQ, "\"": SECONDQ},
+            SECONDQ: {(",", "\"", "\n"): ERROR, ",": START, "\n": RETURN, "\"": FIRSTQ}
+            }
+    state = init_state
+    for line in genarate_lines(filename):
+        if comment and line.startswith("#"):
+            continue
+        for char in line:
+            print(states_table[state], char)
+            transition = transitions[state]
+            for rule, s in transition.items():
+                if isinstance(rule, tuple):
+                    for r in rule:
+                        if char == r:
+                            state = s
+                elif char == rule:
+                    state = s
+
+                if state == ERROR:
+                    return False
+        else:
+            state = init_state
+    return True
+
+
 if __name__ == "__main__":
     if validate_csv("sample.csv", comment=True):
+    # if validate_csv2("sample.csv", comment=True):
         print("accept")
     else:
         print("error")
